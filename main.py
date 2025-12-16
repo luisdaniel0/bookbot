b274d5fb437a318d2b45ce312aa4200bbbe46acb
luisdaniel0 <luisanchezdh@gmail.com>
2025-12-16 15:55:41 -0500

    implemented character_count list and sorted all character counts via descending order, added proper printing statements to each function

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from stats import get_book_text
 
 
 def main():
+    print("============ BOOKBOT ============")
     file_path = "./books/frankenstein.txt"
+    print(f"Analyzing book found at {file_path}")
     get_book_text(file_path)
+    
     
 
 main()
--- stats.py
+++ stats.py
@@ -1,31 +1,61 @@
 def word_count(file):
     split_string = file.split(None)
+    print("----------- Word Count ----------")
     print(f"Found {len(split_string)} total words")
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_content = f.read()
-        character_count(file_content)
+        char_dict = character_count(file_content)
+        word_count(file_content)
+        char_list = convert_to_list(char_dict)
+
+        char_list.sort(reverse=True,key=sort_on)
+        print("--------- Character Count -------")
+        for char in char_list:
+            
+            print(f"{char['char']}: {char['num']}")
+        print("============= END ===============")
         
+
+def convert_to_list(char_dictionary):
+    char_list = []
+    for char in char_dictionary:
+        char_dict = {"char": char, "num": char_dictionary[char]}
+        char_list.append(char_dict)
+
+    return char_list
+
 def sort_on(items):
-    return items
+    return items["num"]
 
 def character_count(string):
     characters = {}
     lower_cased_string = string.lower()
     for char in lower_cased_string:
-        
-        if char in characters:
-            characters[char] +=1
-            print(f"repeated character: {char}")
-            
-        else:
-            characters[char]= 1 
+        if char.isalpha():
+            if char in characters:
+             characters[char] +=1
+            else:
+                characters[char]= 1 
+    return characters
+
+
+
+
+
+
+
+
+
+
+
+
 
     
     
-    characters.sort(reverse=True, key=sort_on)
-    print(characters)
+
+
 
 
 
